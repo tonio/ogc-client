377e45a7f67b1e53be416353cbb81716c74dad70
Antoine Abt <antoine@abt.im>
2025-10-28 09:25:56 +0100

    feat: initial implementation for STAC protocol

---
+++ src/stac/index.ts
@@ -0,0 +1,4 @@
+export { default as StacEndpoint } from './endpoint.js';
+export type { GetCollectionItemsOptions } from './endpoint.js';
+export type { StacEndpointInfo } from './info.js';
+export * from './model.js';
